e441fdebf5f2d5d1e9a5e52b8802408a62152144
Ricky Cheah <49166991+rickycheah@users.noreply.github.com>
2024-02-09 11:34:17 -0500

    PWA and Images (#4)
    * Add favicon
    
    * Add PWA

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,13 +1,18 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
+  manifest: "/manifest.json",
   title: "Energy Efficient Household Rewiring",
   description:
     "Your plan to go electric should be as unique as you are. Now it can be.",
+};
+
+export const viewport: Viewport = {
+  themeColor: "#f9d65b",
 };
 
 export default function RootLayout({
--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
+import withPWAInit from "@ducanh2912/next-pwa";
+
+const withPWA = withPWAInit({
+  dest: "public",
+  cacheOnFrontEndNav: true,
+  aggresiveFrontEndNavCaching: true, // Cache every <link rel="stylesheet" /> and <script /> on frontend navigation
+  reloadOnOnline: true, // Reload the app when it has gone back online
+  disable: false, // want PWA in dev
+  workboxOptions: {
+    disableDevLogs: true,
+  },
+});
+
 const nextConfig = {};
 
-export default nextConfig;
+export default withPWA(nextConfig);
