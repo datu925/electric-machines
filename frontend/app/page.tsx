74c338f0180fe99ea7ce81b452c14f681a8cc56c
Ricky Cheah <49166991+rickycheah@users.noreply.github.com>
2024-02-08 21:12:25 -0500

    Initial Commit - Team Page (#1)
    * Initial Commit - Team Page
    
    * Delete .DS_Store

---
+++ frontend/app/page.tsx
@@ -0,0 +1,170 @@
+import Image from "next/image";
+// import styles from "./page.module.css";
+import styles from "./page.module.scss";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <h1 className={styles.title}>Energy Efficient Household Rewiring</h1>
+      <section>
+        <h2>Project Description</h2>
+        <p>
+          In the next few decades, we&rsquo;ll replace 1 billion household
+          machines for various functions. To help consumers choose and compare
+          these machines, take advantage of tax incentives, and assist
+          contractors in finding the right fit, we need a centralized platform.
+        </p>
+        <p>
+          During the Spring 2024 semester, our team of four is diligently
+          working on this project as part of the Computing for Good course at
+          Georgia Tech. We are collaborating closely with Rewiring America to
+          develop this essential tool.
+        </p>
+      </section>
+      <section>
+        <h2>Project Goals</h2>
+        <p>
+          Create a database and API for household electric appliances such as
+          furnaces, heat pumps, and water heaters, and use it to build a
+          consumer-facing web app.
+        </p>
+        <p>Tasks:</p>
+        <ul>
+          <li>
+            Collect nomenclatures: the organizational structure of appliance
+            model numbers and what they represent.
+          </li>
+          <li>Determine how to represent it in a database.</li>
+          <li>Create a parser that searches the database for matches.</li>
+          <li>
+            Construct a front-end that takes applicance model numbers, finds
+            matches, and unpacks it for a user.
+          </li>
+        </ul>
+      </section>
+      <section>
+        <h2>Team Members</h2>
+        <div className={styles.profileContainer}>
+          <div className={styles.profileImage}>
+            <Image
+              width={64}
+              height={64}
+              style={{
+                objectFit: "cover",
+                borderRadius: "100px",
+              }}
+              src="/team-images/dan.jpeg"
+              alt="Dan Turzca"
+            />
+          </div>
+          <div className={styles.profileContent}>
+            <p className={styles.name}>
+              <strong>Dan Turzca</strong>
+            </p>
+            <p className={styles.title}>Backend Development</p>
+            <p className={styles.description}>
+              More description to come as the project develops!
+            </p>
+            {/* <p className={styles.description}>
+              Description of the role played by Dan Turzca. Lorem, ipsum dolor
+              sit amet consectetur adipisicing elit. Id, libero? Expedita
+              nesciunt excepturi quae dicta, officia aliquam esse iusto,
+              blanditiis eligendi cumque repellat modi distinctio debitis!
+              Fugiat molestias earum optio!
+            </p> */}
+          </div>
+        </div>
+        <div className={styles.profileContainer}>
+          <div className={styles.profileImage}>
+            <Image
+              width={64}
+              height={64}
+              style={{
+                objectFit: "cover",
+                borderRadius: "100px",
+              }}
+              src="/team-images/kevin.jpeg"
+              alt="Kevin Ferri"
+            />
+          </div>
+          <div className={styles.profileContent}>
+            <p className={styles.name}>
+              <strong>Kevin Ferri</strong>
+            </p>
+            <p className={styles.title}>Backend Development</p>
+            <p className={styles.description}>
+              More description to come as the project develops!
+            </p>
+            {/* <p className={styles.description}>
+              Description of the role played by Kevin Ferri. Lorem, ipsum dolor
+              sit amet consectetur adipisicing elit. Id, libero? Expedita
+              nesciunt excepturi quae dicta, officia aliquam esse iusto,
+              blanditiis eligendi cumque repellat modi distinctio debitis!
+              Fugiat molestias earum optio!
+            </p> */}
+          </div>
+        </div>
+        <div className={styles.profileContainer}>
+          <div className={styles.profileImage}>
+            <Image
+              width={64}
+              height={64}
+              style={{
+                objectFit: "cover",
+                borderRadius: "100px",
+              }}
+              src="/team-images/ricky.jpeg"
+              alt="Ricky Cheah"
+            />
+          </div>
+          <div className={styles.profileContent}>
+            <p className={styles.name}>
+              <strong>Ricky Cheah</strong>
+            </p>
+            <p className={styles.title}>Front-End Development</p>
+            <p className={styles.description}>
+              More description to come as the project develops!
+            </p>
+            {/* <p className={styles.description}>
+              Description of the role played by Ricky Cheah. Lorem, ipsum dolor
+              sit amet consectetur adipisicing elit. Id, libero? Expedita
+              nesciunt excepturi quae dicta, officia aliquam esse iusto,
+              blanditiis eligendi cumque repellat modi distinctio debitis!
+              Fugiat molestias earum optio!
+            </p> */}
+          </div>
+        </div>
+        <div className={styles.profileContainer}>
+          <div className={styles.profileImage}>
+            <Image
+              width={64}
+              height={64}
+              style={{
+                objectFit: "cover",
+                borderRadius: "100px",
+              }}
+              src="/team-images/chris.jpeg"
+              alt="Chris Yang"
+            />
+          </div>
+          <div className={styles.profileContent}>
+            <p className={styles.name}>
+              <strong>Chris Yang</strong>
+            </p>
+            <p className={styles.title}>Backend Development</p>
+            <p className={styles.description}>
+              More description to come as the project develops!
+            </p>
+            {/* <p className={styles.description}>
+              Description of the role played by Chris Yang. Lorem, ipsum dolor
+              sit amet consectetur adipisicing elit. Id, libero? Expedita
+              nesciunt excepturi quae dicta, officia aliquam esse iusto,
+              blanditiis eligendi cumque repellat modi distinctio debitis!
+              Fugiat molestias earum optio!
+            </p> */}
+          </div>
+        </div>
+      </section>
+    </main>
+  );
+}
