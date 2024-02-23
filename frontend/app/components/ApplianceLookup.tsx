2523f6967948f52fa280e345d7c98a4ffdd2ab85
Ricky Cheah <49166991+rickycheah@users.noreply.github.com>
2024-02-22 20:56:14 -0500

    Add sample output and survey information

--- frontend/app/components/ApplianceLookup.tsx
+++ frontend/app/components/ApplianceLookup.tsx
@@ -1,17 +1,54 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useState, useMemo } from "react";
 import styles from "./ApplianceLookup.module.scss";
+import TableContainer from "./TableContainer";
 
 const MachineLookup = () => {
   const [selectedAppliance, setSelectedAppliance] = useState("");
   const [showResults, setShowResults] = useState(false);
 
+  const [results, setResults] = useState([
+    {
+      brand: "BrandA",
+      id: 1,
+      model: "GEH50DFEJ2RA",
+      capacity: 60,
+      fhr: 60,
+      uef: 2.6,
+    },
+    {
+      brand: "BrandB",
+      id: 2,
+      model: "HP10-80H42DV",
+      capacity: 70,
+      fhr: 70,
+      uef: 2.7,
+    },
+    {
+      brand: "BrandC",
+      id: 3,
+      model: "RE2H80R10B-12CWT",
+      capacity: 70,
+      fhr: 50,
+      uef: 2.8,
+    },
+    {
+      brand: "BrandD",
+      id: 4,
+      model: "RE22SR10B-12CWT",
+      capacity: 60,
+      fhr: 60,
+      uef: 2.9,
+    },
+  ]);
+
   //default values
-  const [uef, setUef] = useState("1.95");
+  const [uef, setUef] = useState("2.0");
   const [fhr, setFhr] = useState("80");
 
   const handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setShowResults(false);
     setSelectedAppliance(event.target.value);
   };
 
@@ -20,18 +57,50 @@ const MachineLookup = () => {
     setShowResults(true);
   };
 
+  const columns = useMemo(
+    () => [
+      {
+        Header: "Brand",
+        accessor: "brand",
+      },
+      {
+        Header: "Model",
+        accessor: "model",
+      },
+      {
+        Header: "Capacity",
+        accessor: "capacity",
+      },
+      {
+        Header: "UEF",
+        accessor: "uef",
+      },
+      {
+        Header: "FHR",
+        accessor: "fhr",
+      },
+    ],
+    []
+  );
   return (
     <>
+      <label id="appliance-type">Please select the appliance type:</label>
+      <br />
       <select
         className={styles.dropdown}
         value={selectedAppliance}
         onChange={handleChange}
+        id="appliance-type"
+        aria-labelledby="appliance-type"
       >
         <option value="">Select Appliance</option>
         <option value="HeatPumpWaterHeater">Heat Pump Water Heater</option>
         <option value="HeatPumpHVAC">Heat Pump HVAC</option>
         <option value="HeatPumpDryer">Heat Pump Dryer</option>
       </select>
+
+      {selectedAppliance === "HeatPumpHVAC" && <p>Not yet implemented!</p>}
+      {selectedAppliance === "HeatPumpDryer" && <p>Not yet implemented!</p>}
 
       {selectedAppliance === "HeatPumpWaterHeater" && (
         <form className={styles.form} onSubmit={handleSubmit}>
@@ -111,7 +180,7 @@ const MachineLookup = () => {
               name="fhr"
               min="40"
               max="120"
-              step="1"
+              step="10"
               value={fhr}
               onChange={(e) => setFhr(e.target.value)}
               className={styles.slider}
@@ -126,7 +195,12 @@ const MachineLookup = () => {
           </div>
         </form>
       )}
-      {showResults && <div>results</div>}
+      {showResults && (
+        <>
+          {/* <div>{JSON.stringify(results)}</div>*/}
+          <TableContainer columns={columns} data={results} />
+        </>
+      )}
     </>
   );
 };
