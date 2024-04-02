9deb4c795fa63d0d167e4075bb89ae6782694446
Dan Turcza <datu925@gmail.com>
2024-04-02 01:12:29 -0500

    Add validate and generate changes too.

--- pipeline/src/validate_data.ts
+++ pipeline/src/validate_data.ts
@@ -1,4 +1,5 @@
 import Ajv, { ValidateFunction } from "ajv";
+import { SomeJSONSchema } from "ajv/dist/types/json-schema";
 import { program } from "commander";
 import * as _ from "lodash";
 
@@ -8,7 +9,10 @@ import path = require("node:path");
 import { stringify } from "csv-stringify/sync";
 
 import { Table } from "./table_merger";
-import { Appliance } from "../../backend/schema/appliance";
+import {
+  Appliance,
+  HEAT_PUMP_DRYER_SCHEMA,
+} from "../../backend/schema/appliance";
 import { HEAT_PUMP_SCHEMA } from "../../backend/schema/heat_pump";
 import { glob } from "glob";
 import { retrieveMetadata } from "./metadata";
@@ -35,6 +39,9 @@ async function initializeValidators() {
   validators[APPLIANCE_TYPES.heat_pump_water_heater] = ajv.compile(
     HEAT_PUMP_WATER_HEATER_SCHEMA
   );
+  validators[APPLIANCE_TYPES.heat_pump_dryer] = ajv.compile(
+    HEAT_PUMP_DRYER_SCHEMA
+  );
 
   return validators;
 }
@@ -50,6 +57,20 @@ function findMatchingInput(
     }
   }
   return undefined;
+}
+
+function getColumns(records: Table[], schema: SomeJSONSchema): string[] {
+  // Preseed with properties from schema
+  let headers = Object.keys(schema.properties);
+  headers = ["valid"].concat(headers);
+  for (const record of records) {
+    for (const key in record) {
+      if (!headers.includes(key)) {
+        headers.push(key);
+      }
+    }
+  }
+  return headers;
 }
 
 async function main() {
@@ -87,7 +108,7 @@ async function main() {
         }
 
         const metadata = await retrieveMetadata(applianceFolder);
-        const columnsToCopy = ["brandName", "sourceUrl"];
+        const columnsToCopy = ["applianceType", "brandName", "sourceUrl"];
         const metadataToCopy = _.pick(metadata, columnsToCopy);
 
         const specs = Array.isArray(filtered["data"])
@@ -102,27 +123,27 @@ async function main() {
             );
           if (validate(augmentedSpec)) {
             valid.push(augmentedSpec);
-            let spreadsheetRecord = {
-              valid: "true",
-              ...augmentedSpec,
-            };
-            const matchingRecord = findMatchingInput(
-              augmentedSpec.modelNumber,
-              filtered["input"]
-            );
-            if (matchingRecord) {
-              spreadsheetRecord = {
-                ...spreadsheetRecord,
-                ...matchingRecord,
-              };
-            }
-            allRecords.push(spreadsheetRecord);
           } else {
             if (validate.errors !== undefined && validate.errors !== null) {
               augmentedSpec.errors = validate.errors;
             }
             invalid.push(augmentedSpec);
           }
+          let spreadsheetRecord = {
+            valid: validate(augmentedSpec) ? "true" : "false",
+            ...augmentedSpec,
+          };
+          const matchingRecord = findMatchingInput(
+            augmentedSpec.modelNumber,
+            filtered["input"]
+          );
+          if (matchingRecord) {
+            spreadsheetRecord = {
+              ...spreadsheetRecord,
+              ...matchingRecord,
+            };
+          }
+          allRecords.push(spreadsheetRecord);
         }
         const outputFolder = path.join(applianceFolder, OUTPUT_SUBDIR);
         await fs.mkdir(outputFolder, { recursive: true });
@@ -140,9 +161,13 @@ async function main() {
     }
   }
   const ts = Date.now();
+  await fs.mkdir(path.join(SPECS_FILE_BASE, RUNS, ts.toString()));
   await fs.writeFile(
-    path.join(SPECS_FILE_BASE, RUNS, ts.toString()),
-    stringify(allRecords),
+    path.join(SPECS_FILE_BASE, RUNS, ts.toString(), "all_records.csv"),
+    stringify(allRecords, {
+      header: true,
+      columns: getColumns(allRecords, HEAT_PUMP_SCHEMA),
+    }),
     "utf-8"
   );
 }
