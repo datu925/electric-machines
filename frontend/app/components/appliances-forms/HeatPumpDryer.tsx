132999380fcefa81a473a802359bd846c96c1fe5
Ricky Cheah <49166991+rickycheah@users.noreply.github.com>
2024-04-03 14:52:29 -0400

    Add column filters for dryer, minor css changes (#58)

--- frontend/app/components/appliances-forms/HeatPumpDryer.tsx
+++ frontend/app/components/appliances-forms/HeatPumpDryer.tsx
@@ -1,6 +1,6 @@
 import InfoSquare from "../InfoSquare";
 import styles from "./sharedForms.module.scss";
-import { useState, useMemo } from "react";
+import { useState, useMemo, useEffect } from "react";
 import "./tabulator-modern-custom.css";
 // import "react-tabulator/lib/styles.css";
 import { ReactTabulator, ColumnDefinition } from "react-tabulator";
@@ -18,51 +18,127 @@ const HeatPumpDryer = () => {
 
   //sample API results
   // {
-  //   brandName: "Rheem",
-  //   modelNumber: "XJ-75F",
-  //   modelVariant: "701460",
-  //   weightInKg: 71.214,
-  //   widthInCm: 50,
-  //   heightInCm: 160.02,
-  //   lengthInCm: 50,
-  //   electricBreakerSize: 3.67,
-  //   voltage: 120,
-  //   soundLevelMin: 63,
-  //   combinedEnergyFactor: 5,
-  //   capacity: 7,
-  // }
+  //   "brandName": "General Electric",
+  //   "modelNumber": "PFD87ESSVWW/ES(P/M)VRS 240V",
+  //   "sourceUrl": "https://products-salsify.geappliances.com/image/upload/s--XdcWb9Qq--/tjtu3zcktpaufsk10vrm.pdf?_ga=2.104748992.1570831524.1711587971-928897490.1711587971",
+  //   "weightInKg": 50,
+  //   "widthInCm": 50,
+  //   "heightInCm": 160.02,
+  //   "lengthInCm": 50,
+  //   "electricBreakerSize": 30,
+  //   "voltage": 240,
+  //   "soundLevelMax": 63,
+  //   "combinedEnergyFactor": 2,
+  //   "capacity": 7.8
+  // },
 
-  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    const apiUrl = `https://electric-machines-h6x1.vercel.app/api/v1/appliance?applianceType=hpd&soundLevel=${soundLevel}&cef=${combinedEnergyFactor}&capacityMin=${capacity}&capacityMax=100`;
-    console.log(apiUrl);
-    const response = await fetch(apiUrl);
-    const data = await response.json();
-    // console.log(data);
-    setResults(data);
-    setShowResults(true);
-  };
-
+  // const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+  //   event.preventDefault();
+  //   // const apiUrl = `https://electric-machines-h6x1.vercel.app/api/v1/appliance?applianceType=hpd&soundLevel=${soundLevel}&cef=${combinedEnergyFactor}&capacityMin=${capacity}&capacityMax=100`;
+  //   const apiUrl = `https://electric-machines-h6x1.vercel.app/api/v1/appliance?applianceType=hpd&soundLevel=100&cef=1.0&capacityMin=1.0&capacityMax=100`;
+  //   console.log(apiUrl);
+  //   const response = await fetch(apiUrl);
+  //   const data = await response.json();
+  //   // console.log(data);
+  //   setResults(data);
+  //   setShowResults(true);
+  // };
   const [results, setResults] = useState([]);
 
+  const fetchData = async () => {
+    const apiUrl = `https://electric-machines-h6x1.vercel.app/api/v1/appliance?applianceType=hpd&soundLevel=100&cef=1.0&capacityMin=1.0&capacityMax=100`;
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    setResults(data);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   const columns: ColumnDefinition[] = [
-    { title: "Brand", field: "brandName", minWidth: 120 },
-    { title: "Model", field: "modelNumber", hozAlign: "left", minWidth: 120 },
+    {
+      title: "Brand",
+      field: "brandName",
+      minWidth: 165,
+      headerFilter: "input",
+      headerFilterPlaceholder: "Brand (e.g., Rheem)",
+    },
+    {
+      title: "Model",
+      field: "modelNumber",
+      hozAlign: "left",
+      minWidth: 200,
+      headerFilter: "input",
+      headerFilterPlaceholder: "Model (e.g., RH375)",
+    },
     {
       title: "Capacity (cu-ft)",
       field: "capacity",
       hozAlign: "center",
-      minWidth: 150,
+      minWidth: 160,
+      headerFilter: "input",
+      headerFilterFunc: ">=",
+      headerFilterPlaceholder: "Minimum (e.g., 3.0)",
+      headerTooltip:
+        "Capacity refers to the volume of clothes the dryer can hold and dry efficiently, usually measured in cubic feet. A larger capacity is ideal for big households or doing less frequent, larger loads.",
     },
     {
       title: "CEF",
       field: "combinedEnergyFactor",
       hozAlign: "center",
-      minWidth: 100,
+      minWidth: 150,
+      headerFilter: "input",
+      headerFilterFunc: ">=",
+      headerFilterPlaceholder: "Mininum (e.g., 1.0)",
+      headerTooltip:
+        "A higher CEF means better energy efficiency, leading to lower operating costs over time. Consider this factor for long-term savings.",
     },
     {
       title: "Sound Level (dB)",
       field: "soundLevelMax",
+      hozAlign: "center",
+      minWidth: 150,
+      headerFilter: "input",
+      headerFilterFunc: "<=",
+      headerFilterPlaceholder: "Maximum (e.g., 65)",
+      headerTooltip:
+        "<60 dB: Very quiet, ideal for living areas. 60-65 dB: Noticeable, not too loud, common for dryers. >65 dB: Loud, like a vacuum, might be disruptive.",
+    },
+    {
+      title: "Voltage",
+      field: "voltage",
+      hozAlign: "center",
+      minWidth: 140,
+    },
+    {
+      title: "Weight (kg)",
+      field: "weightInKg",
+      hozAlign: "center",
+      minWidth: 140,
+    },
+
+    {
+      title: "Width (cm)",
+      field: "widthInCm",
+      hozAlign: "center",
+      minWidth: 150,
+    },
+    {
+      title: "Height (cm)",
+      field: "heightInCm",
+      hozAlign: "center",
+      minWidth: 150,
+    },
+    {
+      title: "Length (cm)",
+      field: "lengthInCm",
+      hozAlign: "center",
+      minWidth: 150,
+    },
+    {
+      title: "Breaker Size",
+      field: "electricBreakerSize",
       hozAlign: "center",
       minWidth: 150,
     },
@@ -70,7 +146,7 @@ const HeatPumpDryer = () => {
 
   return (
     <>
-      <form className={styles.form} onSubmit={handleSubmit}>
+      {/* <form className={styles.form} onSubmit={handleSubmit}>
         <div className={styles.sliderGroup}>
           <label className={styles.labelWithInfo} htmlFor="capacity-slider">
             <InfoSquare text="Capacity refers to the volume of clothes the dryer can hold and dry efficiently, usually measured in cubic feet. A larger capacity is ideal for big households or doing less frequent, larger loads." />
@@ -178,20 +254,20 @@ const HeatPumpDryer = () => {
           />
         </div>
       </form>
-      {/* layout={"fitData"} */}
+      layout={"fitData"} */}
 
-      {showResults && (
-        <div className={styles.resultTable}>
-          <ReactTabulator
-            data={results}
-            columns={columns}
-            options={{
-              pagination: "local",
-              paginationSize: 8,
-            }}
-          />
-        </div>
-      )}
+      {/* {showResults && ( */}
+      <div className={styles.resultTable}>
+        <ReactTabulator
+          data={results}
+          columns={columns}
+          options={{
+            pagination: "local",
+            paginationSize: 8,
+          }}
+        />
+      </div>
+      {/* )} */}
     </>
   );
 };
