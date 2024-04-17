6e03d32a9470d86db35b1a678bae6ff3096153a0
mellonlord <70414292+mellonlord@users.noreply.github.com>
2024-04-16 21:25:31 -0400

    Made PR changes

--- pipeline/src/convert_csv.ts
+++ pipeline/src/convert_csv.ts
@@ -1,77 +1,66 @@
 import { program } from "commander";
 import util from "util";
 
-import FS from 'node:fs';
+import fs from 'node:fs';
 import assert from 'node:assert';
 import {parse} from 'csv-parse/sync';
 
-import fs = require("node:fs/promises");
 import path = require("node:path");
+
 
 import { Table, filterTables, mergeTablesByModelNumber } from "./table_merger";
 import { APPLIANCE_TYPES } from "../../backend/schema/metadata";
 import { glob } from "glob";
 import { retrieveMetadata } from "./metadata";
 
+const _ = require("lodash");
+
 const SPECS_FILE_BASE = "../data/";
 const INPUT_SUBDIR = "reformatted/";
 const OUTPUT_SUBDIR = "";
 const RUNS = "runs/";
 
+program.requiredOption(
+    "-i, --input_file <file>",
+    "Path to input file with appliance metadata."
+  );
+
 program.parse();
 
 async function main() {
-// loop through the sub folders in the run folder
-const rfolder = path.join(SPECS_FILE_BASE, RUNS).replace(/\\/g, '/');
-FS.readdir(rfolder, async (err, topFolders) => {
-    if (err || !topFolders.length){
-        console.log(`No runs detected`);
-    }
-    else{
-        for (const file of topFolders){
-            // Try to open the "all_records.csv" file
-            const runFile = path.join(rfolder, file, "all_records.csv");
-            console.log(`Trying ${runFile}`);
-            FS.readFile(runFile, function (err, content) {
-                // Skip the folder if there is no file
-                if(err){
-                    console.log(`Skipping ${file} because it is empty`);
-                }
-                else{
-                    //Parse content and convert to JSON
-                    console.log(`Parsing file found in ${runFile}`);
-                    const records = parse(content, {
-                        skip_empty_lines: true,
-                        columns: true
-                    });
-                    // Create raw data file
-                    const rawFileName = path.join(rfolder, file, "all_records.JSON").replace(/\\/g, '/');
-                    const recordVals = Object.values(records);
-                    const jsonFormatRecords = JSON.stringify(recordVals);
+    // loop through the given folder
+    const rfolder = program.opts();
+    //console.log(`Input File: ${rfolder.input_file}`);
+    const runFile = fs.readFileSync(rfolder.input_file, {encoding: "utf-8"});
+                
+    //Parse content and convert to JSON
+    const records = parse(runFile, {
+        skip_empty_lines: true,
+        columns: true
+    });
 
-                    fs.writeFile(rawFileName, jsonFormatRecords, "utf-8");
+    // Create raw data file
+    const rawFileName = path.join(rfolder.input_file, "../" , "all_records.json").replace(/\\/g, '/');
+    console.log(`Number of Records found: ${records.length}`);
+    fs.writeFileSync(rawFileName, JSON.stringify(records),  {
+        encoding: "utf-8",
+        flag: "w",
+        });
 
-                    // Create file for each type of appliance JSON key: "applianceType"
-                    // "applianceType"s key values pulled from the metadata file
-                    for (const appliance of Object.values(APPLIANCE_TYPES)){
-                        // Iterate through JSON data keeping only entries with the matching key
-                        let subSet = [];
-                        for (const entry of records){
-                            if (entry["applianceType"] == appliance){
-                                subSet.push(entry);
-                            }
-                        }
-                        // Save the JSON data for the single appliance type
-                        let fileName = appliance.concat(".JSON");
-                        let subSetFilePath = path.join(rfolder, file, fileName).replace(/\\/g, '/');
-                        let jsonSubSet = JSON.stringify(subSet);
-                        fs.writeFile(subSetFilePath, jsonSubSet, "utf-8");
-                    }
-                }
+    // Create file for each type of appliance JSON key: "applianceType"
+    // "applianceType"s key values pulled from the metadata file
+    const appliances = Object.values(APPLIANCE_TYPES);
+    for (const appliance of appliances){
+        let fRecords = _.filter(records, ["applianceType", appliance]);
+
+        // Save the JSON data for the single appliance type
+        let fileName = appliance.concat(".json");
+        let subSetFilePath = path.join(rfolder.input_file, "../", fileName).replace(/\\/g, '/');
+        fs.writeFileSync(subSetFilePath, JSON.stringify(fRecords),  {
+            encoding: "utf-8",
+            flag: "w",
             });
-        }
     }
-});
 }
 
 main();
